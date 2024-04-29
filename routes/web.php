d190979996a684ef27b45bb9adf068e508984269
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-29 15:45:30 +0600

    Conversation ladder

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,46 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $messages = [
+        [
+            "role" => "system",
+            "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+        ],
+        [
+            "role" => "user",
+            "content" => "Compose a poem that explains the concept of recursion in programming."
+        ]
+    ];
+
     $poem = Http::withToken(config('services.openai.secret'))
         ->post('https://api.openai.com/v1/chat/completions',
             [
                 "model" => "gpt-3.5-turbo-0125",
-                "messages" => [
-                    [
-                        "role" => "system",
-                        "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
-                    ],
-                    [
-                        "role" => "user",
-                        "content" => "Compose a poem that explains the concept of recursion in programming."
-                    ]
-                ]
+                "messages" => $messages
             ])->json('choices.0.message.content');
 
-    return view('welcome', ['poem' => $poem]);
+    $messages[] = [
+        'role' => 'assistant',
+        'content' => $poem
+    ];
+
+    $messages[] = [
+        'role' => 'user',
+        'content' => 'Good, but can you make it much, much more silly.',
+    ];
+
+    $sillyPoem = Http::withToken(config('services.openai.secret'))
+        ->post('https://api.openai.com/v1/chat/completions',
+            [
+                "model" => "gpt-3.5-turbo-0125",
+                "messages" => $messages
+            ])->json('choices.0.message.content');
+
+    $messages[] = [
+        'role' => 'assistant',
+        'content' => $sillyPoem
+    ];
+
+
+    return view('welcome', ['poem' => $sillyPoem]);
 });
