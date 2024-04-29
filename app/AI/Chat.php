40899693a72c00a920d2cf1a9f8c7cdf934f1625
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-29 16:21:58 +0600

    Handling conversation as Chat object class

---
+++ app/AI/Chat.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\AI;
+
+use Illuminate\Support\Facades\Http;
+
+class Chat
+{
+    protected array $messages = [];
+
+    public function systemMessage(string $message): static
+    {
+        $this->messages[] = [
+            'role' => 'system',
+            'content' => $message,
+        ];
+
+        return $this;
+    }
+
+    public function send(string $message): ?string
+    {
+        $this->messages[] = [
+            'role' => 'user',
+            'content' => $message
+        ];
+
+        $response = Http::withToken(config('services.openai.secret'))
+            ->post('https://api.openai.com/v1/chat/completions',
+                [
+                    "model" => "gpt-3.5-turbo-0125",
+                    "messages" => $this->messages
+                ])->json('choices.0.message.content');
+
+        if ($response){
+            $messages[] = [
+                'role' => 'assistant',
+                'content' => $response
+            ];
+        }
+
+        return $response;
+    }
+
+    public function reply(string $message):?string
+    {
+        return $this->send($message);
+    }
+
+    public function messages()
+    {
+        return $this->messages;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,18 @@
 <?php
 
+use App\AI\Chat;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $messages = [
-        [
-            "role" => "system",
-            "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
-        ],
-        [
-            "role" => "user",
-            "content" => "Compose a poem that explains the concept of recursion in programming."
-        ]
-    ];
 
-    $poem = Http::withToken(config('services.openai.secret'))
-        ->post('https://api.openai.com/v1/chat/completions',
-            [
-                "model" => "gpt-3.5-turbo-0125",
-                "messages" => $messages
-            ])->json('choices.0.message.content');
+    $chat = new Chat();
 
-    $messages[] = [
-        'role' => 'assistant',
-        'content' => $poem
-    ];
+    $poem = $chat
+        ->systemMessage('You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.')
+        ->send('Compose a poem that explains the concept of recursion in programming.');
 
-    $messages[] = [
-        'role' => 'user',
-        'content' => 'Good, but can you make it much, much more silly.',
-    ];
+    $newPoem = $chat->reply('Good, but can you make it much, much more silly.');
 
-    $sillyPoem = Http::withToken(config('services.openai.secret'))
-        ->post('https://api.openai.com/v1/chat/completions',
-            [
-                "model" => "gpt-3.5-turbo-0125",
-                "messages" => $messages
-            ])->json('choices.0.message.content');
-
-    $messages[] = [
-        'role' => 'assistant',
-        'content' => $sillyPoem
-    ];
-
-
-    return view('welcome', ['poem' => $sillyPoem]);
+    return view('welcome', ['poem' => $newPoem]);
 });
