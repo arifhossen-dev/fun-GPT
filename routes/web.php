29e0d4f08df31f9b99016bcbd26f2b93f74406d0
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-29 15:38:59 +0600

    Say hallo to OpenAi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $poem = Http::withToken(config('services.openai.secret'))
+        ->post('https://api.openai.com/v1/chat/completions',
+            [
+                "model" => "gpt-3.5-turbo-0125",
+                "messages" => [
+                    [
+                        "role" => "system",
+                        "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => "Compose a poem that explains the concept of recursion in programming."
+                    ]
+                ]
+            ])->json('choices.0.message.content');
+
+    return view('welcome', ['poem' => $poem]);
 });
