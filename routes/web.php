ea94100b4c538ed37519372c025d39c59b9c66f3
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-19 00:03:25 +0600

    Feed OpenAI Our Docs

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,51 @@ use App\AI\Assistant;
 use App\Rules\SpamFree;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use OpenAI\Laravel\Facades\OpenAI;
+
+Route::get('/assistant', function () {
+    $file = OpenAI::files()->upload([
+        'purpose' => 'assistants',
+        'file' => fopen(storage_path('docs/parsing.md'), 'rb')
+    ]);
+
+    $assistant = OpenAI::assistants()->create([
+        'model' => 'gpt-4-turbo-preview',
+        'name' => 'Larvify Tutor',
+        'instructions' => 'You are a helpful programming teacher.',
+        'tools' => [
+            ['type' => 'retrieval']
+        ],
+        'file_ids' => [
+            $file->id
+        ],
+    ]);
+
+    $run = OpenAI::threads()->createAndRun([
+        'assistant_id' => $assistant->id,
+        'thread' => [
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => 'How do I grab the first paragraph?',
+                ]
+            ]
+        ]
+    ]);
+
+    do {
+        sleep(1);
+
+        $run = OpenAI::threads()->runs()->retrieve(
+            threadId: $run->threadId,
+            runId: $run->id
+        );
+    } while ($run->status !== 'completed');
+
+    $messages = OpenAI::threads()->messages()->list($run->threadId);
+
+    dd($messages);
+});
 
 Route::get('replies', function () {
     return view('replay');
